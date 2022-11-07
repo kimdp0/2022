04f034644a8263dc1d58c9fc72accd4883dd918b
SilverCastle123 <chis7890@naver.com>
2022-11-07 16:18:46 +0900

    STS 연동 실험

---
+++ SGLINK/src/main/java/com/sglink/SglinkApplication.java
@@ -0,0 +1,13 @@
+package com.sglink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SglinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SglinkApplication.class, args);
+	}
+
+}
---
+++ SGLINK/src/test/java/com/sglink/SglinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sglink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SglinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
