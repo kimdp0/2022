80fb9b773b618f43a47f0a80e82eee14de287285
dohyeon <kokunii44@gmail.com>
2022-11-21 17:48:54 +0900

    기업지원-인프라페이지 제작

--- SGLINK/src/main/java/com/sglink/controller/InfraController.java
+++ SGLINK/src/main/java/com/sglink/controller/InfraController.java
@@ -7,8 +7,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/comsup")
 @Controller
 public class InfraController {
-	@RequestMapping(value = "/infra/application", method = RequestMethod.GET)
+	@RequestMapping(value = "/infra/introduce", method = RequestMethod.GET)
 	public String introduce() {
+		return "/board/comsup/infra/introduce";
+	}
+	@RequestMapping(value = "/infra/application", method = RequestMethod.GET)
+	public String application() {
 		return "/board/comsup/infra/application";
+	}
+	@RequestMapping(value = "/infra/howto", method = RequestMethod.GET)
+	public String howto() {
+		return "/board/comsup/infra/howto";
+	}
+	@RequestMapping(value = "/infra/onlineapplication", method = RequestMethod.GET)
+	public String onlineapplication() {
+		return "/board/comsup/infra/onlineapplication";
+	}
+	@RequestMapping(value = "/infra/precaution", method = RequestMethod.GET)
+	public String precaution() {
+		return "/board/comsup/infra/precaution";
 	}
 }
