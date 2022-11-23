705a2d3abf02973d0b52d7030b9d7e157d11f0b3
dohyeon <kokunii44@gmail.com>
2022-11-23 14:43:20 +0900

    .

---
+++ SGLINK/src/main/java/com/sglink/repository/EquipmentRepository.java
@@ -0,0 +1,9 @@
+package com.sglink.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sglink.entity.Equipment;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, String>{
+	
+}
