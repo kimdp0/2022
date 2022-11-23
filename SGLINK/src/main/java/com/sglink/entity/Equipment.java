976d4ca0f63eec5934be5473a4de34c4a1b7f0e1
dohyeon <kokunii44@gmail.com>
2022-11-23 15:08:16 +0900

    Merge branch 'sub' of https://github.com/SilverCastle123/project.git into sub

--- SGLINK/src/main/java/com/sglink/entity/Equipment.java
+++ SGLINK/src/main/java/com/sglink/entity/Equipment.java
@@ -10,6 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,16 +28,29 @@ public class Equipment {
 	private String equUniname;
 
 	private String equiContent;
-	
+
 	@OneToOne
-	@JoinColumn(name="img")
+	@JoinColumn(name = "img")
 	private FileEntity file;
 
 	@OneToMany(mappedBy = "equipment")
 	private List<Member> member;
-	
+
 	@ManyToOne
 	@JoinColumn
 	private Company company;
+
+	@Builder
+	public Equipment(String equiId, String equiName, String equUniname, String equiContent, FileEntity file, List<Member> member,
+			Company company) {
+
+		this.equiId = equiId;
+		this.equiName = equiName;
+		this.equUniname = equUniname;
+		this.equiContent = equiContent;
+		this.file = file;
+		this.member = member;
+		this.company = company;
+	}
 
 }
