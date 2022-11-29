df37b26bbf4ccf5601e99f0f580fea4f66358966
BIG205 <BIG205@DESKTOP-CKHDFJ5>
2022-11-29 11:02:39 +0900

    탭메뉴 수정

--- SGLINK/src/main/resources/static/js/tabmenu.js
+++ SGLINK/src/main/resources/static/js/tabmenu.js
@@ -1,16 +1,16 @@
-let tabHeader1 = document.getElementsByClassName("tab-header")[0];
-let tabIndicator1 = document.getElementsByClassName("tab-indicator")[0];
-let tabBody1 = document.getElementsByClassName("tab-body")[0];
+let tabHeader2 = document.getElementsByClassName("tab-header")[0];
+let tabIndicator2 = document.getElementsByClassName("tab-indicator")[0];
+let tabBody2 = document.getElementsByClassName("tab-body")[0];
 
-let tabsPane1 = tabHeader1.getElementsByTagName("div");
+let tabsPane2 = tabHeader2.getElementsByTagName("div");
 
-for(let i=0;i<tabsPane1.length;i++){
-  tabsPane1[i].addEventListener("click",function(){
-    tabHeader1.getElementsByClassName("active")[0].classList.remove("active");
-    tabsPane1[i].classList.add("active");
-    tabBody1.getElementsByClassName("active")[0].classList.remove("active");
-    tabBody1.getElementsByTagName("div")[i].classList.add("active");
+for(let i=0;i<tabsPane2.length;i++){
+  tabsPane2[i].addEventListener("click",function(){
+    tabHeader2.getElementsByClassName("active")[0].classList.remove("active");
+    tabsPane2[i].classList.add("active");
+    tabBody2.getElementsByClassName("active")[0].classList.remove("active");
+    tabBody2.getElementsByTagName("div")[i].classList.add("active");
     
-    tabIndicator1.style.left = `calc(calc(100% / 4) * ${i})`;
+    tabIndicator2.style.left = `calc(calc(100% / 4) * ${i})`;
   });
 }
