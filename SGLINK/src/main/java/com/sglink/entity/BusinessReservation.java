9786ae4a4339c2892f31812ee51256fd90b167bc
dohyeon <kokunii44@gmail.com>
2022-12-05 19:43:19 +0900

    Merge branch 'sub' of https://github.com/SilverCastle123/project.git into sub

--- SGLINK/src/main/java/com/sglink/entity/BusinessReservation.java
+++ SGLINK/src/main/java/com/sglink/entity/BusinessReservation.java
@@ -1,7 +1,5 @@
 package com.sglink.entity;
 
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -26,46 +24,43 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
-public class BusinessReservation{
+public class BusinessReservation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String busiId;
 
 	@Column(nullable = false)
 	private String busiName;
-	
+
 	@Column(nullable = false)
 	private String busiRegisterId;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "userId")
 	private Member member;
 
-	
 	@Enumerated(EnumType.STRING)
 	private Process process;
-	
-	/*private String startDate;
-	private String endDate; */
+
+	private String startDate;
+	private String endDate;
 
 	@Builder
-	public BusinessReservation(String busiId, String busiName ,Member member,
-			/*String startDate,
-			String endDate,*/
-			Process process,
-			String busiRegisterId) {
+	public BusinessReservation(String busiId, String busiName, Member member, String startDate, String endDate,
+			Process process, String busiRegisterId) {
 
 		this.busiId = busiId;
 		this.busiName = busiName;
 		this.member = member;
 		this.process = process;
-		/*this.startDate = startDate;
-		this.endDate  = endDate;*/
+		this.startDate = startDate;
+		this.endDate = endDate;
 		this.busiRegisterId = busiRegisterId;
 	}
+
 	@PrePersist
 	public void prePersist() {
 		this.process = this.process == null ? Process.UNAPPROVE : this.process;
--- SGLINK/src/main/java/com/sglink/member/dto/BusinessReservationResponseDto.java
+++ SGLINK/src/main/java/com/sglink/member/dto/BusinessReservationResponseDto.java
@@ -1,12 +1,12 @@
 package com.sglink.member.dto;
 
-
 import com.sglink.common.constant.Process;
 import com.sglink.entity.BusinessReservation;
 import com.sglink.entity.Member;
 
 import lombok.Getter;
 import lombok.ToString;
+
 @ToString
 @Getter
 public class BusinessReservationResponseDto {
@@ -15,22 +15,21 @@ public class BusinessReservationResponseDto {
 	private String busiName;
 	private String busiRegisterId;
 	private Member member;
-	/*private String startDate; 
-	  private String endDate;
-	 */
+	private String startDate;
+	private String endDate;
+
 	private Process busiProcess;
-	
+
 	public BusinessReservationResponseDto(BusinessReservation entity) {
 		this.id = entity.getId();
-		this.busiId=entity.getBusiId();
-		this.busiName=entity.getBusiName();
-		this.busiRegisterId=entity.getBusiRegisterId();
-		this.member=entity.getMember();
-		/* this.startDate=entity.getStartDate(); 
-		   this.endDate=entity.getEndDate();
-		 */
-		this.busiProcess =entity.getProcess();
+		this.busiId = entity.getBusiId();
+		this.busiName = entity.getBusiName();
+		this.busiRegisterId = entity.getBusiRegisterId();
+		this.member = entity.getMember();
+		this.startDate = entity.getStartDate();
+		this.endDate = entity.getEndDate();
+
+		this.busiProcess = entity.getProcess();
 	}
-	
 
 }
