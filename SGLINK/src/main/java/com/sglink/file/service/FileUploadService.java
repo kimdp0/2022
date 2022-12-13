115c44ffcce66936b8fc8ae1eb856a7cebd1e682
big310 <big310@big310>
2022-12-13 14:43:53 +0900

    파일보드 조금 수정

--- SGLINK/src/main/java/com/sglink/file/service/FileUploadService.java
+++ SGLINK/src/main/java/com/sglink/file/service/FileUploadService.java
@@ -26,7 +26,7 @@ public class FileUploadService {
 
 	
 	public void addFile(List<MultipartFile> files, Long id ,FileBoard fileBoard) throws Exception {
-		// 파일을 저장하고 그 BoardPicture 에 대한 list 를 가지고 있는다
+		// 파일을 저장하고 그 Board 에 대한 list 를 가지고 있는다
 		List<FileEntity> filelist = fileHandler.parseFileInfo(files,id,fileBoard);
 		if (filelist.isEmpty()) {
 			// TODO : 파일이 없을 땐 어떻게 해야할까.. 고민을 해보아야 할 것
